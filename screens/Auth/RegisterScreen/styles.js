d53b7970afee30daf2187b2b43711c95e5f5e554
petruselgeorge1 <petruselgeorge@gmail.com>
2025-05-21 17:46:52 +0300

    authentication done

--- screens/Auth/RegisterScreen/styles.js
+++ screens/Auth/RegisterScreen/styles.js
@@ -3,7 +3,7 @@
 export const successStyles = {
   container: {
     flex: 1,
-    backgroundColor: "#264653",
+    backgroundColor: "rgb(22, 34, 51)",
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 20,
@@ -16,12 +16,12 @@ export const successStyles = {
   title: {
     fontSize: 36,
     fontWeight: "bold",
-    color: "#E9C46A",
+    color: "rgb(255, 255, 255)",
     marginBottom: 20,
   },
   message: {
     fontSize: 18,
-    color: "#E9C46A",
+    color: "rgb(255,255,255)",
     textAlign: "center",
     marginBottom: 40,
   },
